df75149cbfe4ad5d1f78ee25bcab4e3127926cce
ryan dowler <Ryandowler9@hotmail.com>
2017-11-06 17:16:12 +0000

    More testing - model specs

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -10,3 +10,37 @@ feature "visit the homepage" do
    	expect(page).to have_text("Request CV")
   end
 end
+
+RSpec.describe Projecto do
+	describe "#num_of_projects" do
+		it "returns an int" do
+			expect(subject.num_of_projects).to be_a(Numeric)
+		end
+	end
+
+	describe "#feed" do
+		context "when the dog is hungry" do
+			subject { described_class.new(hunger_level: 7)}
+			it "is no longer hungry" do
+				subject.feed
+				expect(subject).to_not be_hungry
+			end	
+		end
+	end
+
+	describe '#hungry?' do
+		context'when hunger_level is more than 5' do
+			subject { described_class.new(hunger_level: 7) }
+			it "returns true " do
+				expect(subject).to be_hungry
+			end
+		end
+		context 'when hunger_level is 5 or less' do
+			subject {described_class.new(hunger_level: 5)}
+			it "returns false" do
+				expect(subject).not_to be_hungry
+			end
+		end
+	end
+end
+
