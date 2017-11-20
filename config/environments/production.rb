b1fc2cf4f29f291324b1be8b6d307e97fd8c1939
ryan dowler <Ryandowler9@hotmail.com>
2017-11-20 23:34:46 +0000

    mailer heroku work - ignore3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,16 +47,16 @@ Rails.application.configure do
   # config.force_ssl = true
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'ryandowler.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://ryandowler.herokuapp.com' }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true 
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.sendgrid.com",
+   :address              => "smtp.sendgrid.net",
    :port                 => 587,
    :authentication       => :plain,
    :user_name            => ENV['SENDGRID_USERNAME'],
    :password             => ENV['SENDGRID_PASSWORD'],
-   :domain => 'herokuapp.com',
+   :domain => 'ryandowler.herokuapp.com',
    :enable_starttls_auto => true
   }
 
